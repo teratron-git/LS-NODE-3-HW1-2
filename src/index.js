042971ef45ab677dd65e74c03cb9bd17d312ea9b
Tereshkov Sergei[D] <rat87@mail.ru>
2020-05-29 22:06:37 +0300

    feat: search files and folders

---
+++ src/index.js
@@ -0,0 +1,106 @@
+const fs = require('fs')
+const path = require('path')
+
+let src = path.join(__dirname, 'DATA');
+let dest = path.join(__dirname, 'SORTED');
+console.log('src', src)
+console.log('dest', dest)
+
+let isFile = (currentFile, cb) => {
+	let isCurrentFile;
+	fs.stat(currentFile, (err, stats) => {
+		if (err) {
+			console.error('!ОШИБКА', err)
+			cb(err, null)
+			return
+		} else {
+			isCurrentFile = stats.isFile()
+			cb(err, isCurrentFile)
+		}
+	})
+	return isCurrentFile;
+}
+
+let data = []
+let len;
+
+function createFolder(path) {
+	fs.access(path, err => {
+		if (!err) {
+			console.log('OK')
+		} else {
+			console.log('!!!err', err)
+			fs.mkdir(path, err => {
+				if (err) {
+					console.error(err)
+					return
+				}
+				console.log('Папка создана')
+			})
+		}
+	});
+}
+
+function readFolder(src) {
+	fs.readdir(src, (err, files) => {
+		if (err) {
+			console.log(err)
+			process.exit(1)
+		}
+		len = files.length
+		files.map(file => {
+			let currentFile = path.join(src, file)
+
+			isFile(currentFile, (err, isCurrentFile) => {
+				if (err) console.log(err);
+				data.push({ file, isCurrentFile })
+				len--
+				if (!isCurrentFile) {
+					console.log('ПАПКА', file)
+
+					readFolder(path.join(src, file))
+				} else {
+					console.log('ФАЙЛ', file)
+					createFolder(path.join(dest, file[0].toUpperCase()))
+				}
+			})
+
+			// console.log(`${currentFile} - ${isCurrentFile}`)
+		})
+	})
+	// createFolder(dest);
+}
+
+createFolder(dest)
+readFolder(src)
+
+
+
+
+// const interval = setInterval(() => {
+// 	if (len === 0) {
+// 		createFolder(dest)
+// 		clearInterval(interval)
+// 	}
+// }, 0)
+
+
+
+// const interval = setInterval(() => {
+// 	if (len === 0) {
+// 		console.log(data)
+// 		clearInterval(interval)
+// 	}
+// }, 100)
+
+
+
+// let copy = (src, dest, file) => {
+// 	fs.copyFile(path.join(src, file), path.join(dest, file), (err) => {
+// 		if (err) {
+// 			console.error(err)
+// 			return
+// 		}
+// 		console.log('Файл скопирован');
+// 	});
+// }
